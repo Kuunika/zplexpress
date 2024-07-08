07abe66dbb6da4f3f4487396f71325a16639c49a
Louis Nyirongo <louisnyirongo73@gmail.com>
2024-07-08 15:01:43 +0200

    add cors

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { exec } = require('child_process');
+const cors = require('cors');
 
 const app = express();
 const port = 3000; 
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
